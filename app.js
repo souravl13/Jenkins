216374c97bacc7f93f729444c04b5909f5fd15f7
souravl13 <souravl0701@gmail.com>
2025-08-11 15:57:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// app.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins Docker Pipeline!');
+  res.send(`Hello from Jenkins Docker Pipeline! ENVIRONMENT=${process.env.ENVIRONMENT}, NODE_ENV=${process.env.NODE_ENV}`);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
+  console.log(`ENVIRONMENT: ${process.env.ENVIRONMENT}`);
+  console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 });
